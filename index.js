ee483df0cdb1b536b6af700dd748822f460369f0
ArkadiKorotitshTARpv22 <gentletin62@gmail.com>
2024-11-05 15:40:06 +0200

    As an app developer i want to be able to add new games [Delivers #188366358]

--- index.js
+++ index.js
@@ -1,12 +1,64 @@
-const app = require('express')()
-
+//api
+// const app = require('express')()
+const express = require('express')
+const app = express()
 const port = 8080
 const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./docs/swagger.json');
+// const swaggerDocument = require('./docs/swagger.json');
+const yamljs = require('yamljs')
+const swaggerDocument = yamljs.load('./docs/swagger.json')
+
+const games = [
+    {id: 1, name: "Lethal Company", price: 9.99},
+    {id: 2, name: "Content Warning", price: 7.99},
+    {id: 3, name: "Titanfall 2", price: 59.99},
+    {id: 4, name: "Roblox", price: 0},
+    {id: 5, name: "Spacemarine 2", price: 59.99},
+    {id: 6, name: "Dawn Of War", price: 29.99},
+    {id: 7, name: "Liar's Bar", price: 7.99},
+    {id: 8, name: "Helldivers 2", price: 39.99},
+
+
+]
 
 app.get('/games', (req, res) => {
-    res.send(["Lethal Company", "Content Warning"])
+    res.send(games)
 
+})
+
+app.get('/games/:id',  (req, res) => {
+    if (typeof games[req.params.id - 1] === 'undefined') {
+        return res.status(404).send({error: "Game not found"})
+    }
+
+    res.send(games[req.params.id - 1])
+})
+
+app.post('/games', (req, res) => {
+    if (!req.body.name || !req.body.price) {
+
+        return res.status(400).send({error: 'One or all params are missing'})
+    }
+    let game = {
+        id: games.length +1,
+        price: req.body.price,
+        name: req.body.name
+    }
+
+    games.push(game)
+    res.status(201).location(`${getBaseUrl(req)}/games/${games.length}`).send(game)
+
+    res.end()
+})
+
+app.delete('/game/:id', (req, res) => {
+    if (typeof games[req.params.id - 1] === 'undefined') {
+        return res.status(404).send({error: "Game not found"})
+    }
+
+    game.splice(req.params.id -1, 1)
+
+    res.status(204).send({error: "No content"})
 })
 
 
@@ -16,3 +68,9 @@ app.listen(port, () => {
 
     console.log(`API up at: http://localhost:${port}`)
 })
+
+
+function getBaseUrl(req) {
+    return req.connection && req.connection.encrypted
+        ? 'https' : 'http' + `://${req.headers.host}`
+}
